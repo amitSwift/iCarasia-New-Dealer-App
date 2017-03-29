39475d786534e4402aac6fced6603d2f2e841efb
Raman Kant <raman@TSPL-PC-92.local>
2017-03-29 17:42:51 +0530

    05:42 PM -- RK

--- iCarasia New Car Dealer App/Controllers/ManageDShipsViewController.swift
+++ iCarasia New Car Dealer App/Controllers/ManageDShipsViewController.swift
@@ -21,7 +21,6 @@ class ManageDShipsViewController: UIViewController   , UITableViewDelegate , UIT
 
         // Do any additional setup after loading the view.
         
-        // amit kumar verma
         self.title = "MANAGE DEALERSHIPS"
         self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
         /*
